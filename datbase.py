3e349ccd360936f34d7029dd3b7d12c8458afba5
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-24 22:55:22 +0530

    Update datbase.py

--- datbase.py
+++ datbase.py
@@ -29,27 +29,29 @@ def importing(subject,date,period):
 
 
 
-#to generate summary
-# list=db.collection("Notes").document("Chemistry").collection("23-09-2023").document("1").get()
-# data=list.to_dict()
-# print(data)
-# if 'summary' in data:
-#       print(data["summary"])
-    
-# else:
-#     def prompt():
-#         llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1)
+# to generate summary
+def to_summ(value,subject,date,time):
+    while (value==True):
+        list=db.collection("Notes").document(subject).collection(date).document(time).get()
+        data=list.to_dict()
+        print(data)
+        if 'summary' in data:
+            print(data["summary"])
+            
+        else:
+            def prompt():
+                llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1)
 
-#         prompt = PromptTemplate(
-#             input_variables=["summ"],
-#             template="write a detailed summary of {summ} and explain it in a very understandable way.",
-#         )
-#         chain = LLMChain(llm=llm, prompt=prompt)
-#         return chain
+                prompt = PromptTemplate(
+                    input_variables=["summ"],
+                    template="write a detailed summary of {summ} and explain it in a very understandable way.",
+                )
+                chain = LLMChain(llm=llm, prompt=prompt)
+                return chain
 
-#     def question1(inputs):
-#         chain = prompt()
-#         return chain.run(inputs)
-#     data1=question1(data['transcribe'])
-#     db.collection("Notes").document("Chemistry").collection("23-09-2023").document("1").update({"summary":data1})
-#     print("done")
+            def question1(inputs):
+                chain = prompt()
+                return chain.run(inputs)
+            data1=question1(data['transcribe'])
+            db.collection("Notes").document(subject).collection(date).document(time).update({"summary":data1})
+    return "done"
