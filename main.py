256a0ea33f787b2efc21d80c36c7a171bd51e071
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-24 22:55:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import *
-import studiesy
+import studiesy 
+import datbase
 app=Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/bot",methods=['POST'])
 def index():
     user_question=request.json['question']
     subject=request.json['subject']
     date=request.json['date']
     period=request.json['period']
     r=studiesy.user(user_question,subject,date,period)
+    print(r)
+    return({"result":r})
+
+@app.route("/summary",methods=['POST'])
+def index():
+    yes=request.json['yes']
+    subject=request.json['subject']
+    date=request.json['date']
+    period=request.json['period']
+    r=datbase.to_summ(yes,subject,date,period)
     print(r)
     return({"result":r})
 
