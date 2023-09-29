444b7173538b97ba6165b0e3178826264880fd5e
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-29 22:14:47 +0530

    Update datbase.py

--- datbase.py
+++ datbase.py
@@ -25,40 +25,14 @@ def importing(subject):
         return ("there was no period today")
 
 
+def chat(res,time):
+    data2={"message":res,"isSender":False,"messageStatus":"viewed","messageType":"text","time":time}
+    list=db.collection("chatroom").document("Alfred").collection("chats").document().get()
+    if list.exists:  
+        db.collection("chatroom").document("Alfred").collection("chats").document().update(data2)
+    else:
+        db.collection("chatroom").document("Alfred").collection("chats").document().set(data2)
 
 
 
-
-
-# to generate summary
-def to_summ(value,subject):
-    reuslt=""
-    while (value=="True"):
-        list=db.collection("Notes").document(subject).get()
-        data=list.to_dict()
-        result1=""
-        if 'summary' in data:
-            result1=data["summary"]
-            print(result1)
-            
-        else:
-            def prompt():
-                llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1)
-
-                prompt = PromptTemplate(
-                    input_variables=["summ"],
-                    template="write a detailed summary of {summ} and explain it in a very understandable way.",
-                )
-                chain = LLMChain(llm=llm, prompt=prompt)
-                return chain
-
-            def question1(inputs):
-                chain = prompt()
-                return chain.run(inputs)
-            data1=question1(data['transcribe'])
-            db.collection("Notes").document(subject).update({"summary":data1})
-            result1=data1
-            print(result1)
-        value="False"
-    return result1
 
