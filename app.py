1a13db109ec7c814b277bab466d5365a29c09a26
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2024-02-28 21:32:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import *
+import studiesy 
+import datbase
+app=Flask(__name__)
+
+@app.route("/bot",methods=['POST'])
+def index():
+    user_question=request.json['question']
+    subject=request.json['subject']
+    # time=request.json['time']
+    # period=request.json['period']
+    r=studiesy.user(user_question,subject)
+    print(r)
+    # datbase.chat(r,time,subject)
+    return({"result":r})
+
+if __name__=="__main__":
+    app.run(debug=True,port=6000)
